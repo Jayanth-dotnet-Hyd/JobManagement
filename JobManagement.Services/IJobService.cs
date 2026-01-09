2c0d6726f6ec5987150ceba0bce42dd96205760f
Jayanth Bandi <bandijayanthh@gmail.com>
2026-01-09 18:19:47 +0530

    Add auth controller and authentication flow

--- JobManagement.Services/IJobService.cs
+++ JobManagement.Services/IJobService.cs
@@ -1,4 +1,5 @@
-﻿using System;
+﻿using JobManagement.Applicant.Data.Models;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
@@ -8,6 +9,13 @@ namespace JobManagement.Services
 {
     public interface IJobService
     {
+
+        Task<IEnumerable<job>> GetAllJobsAsync();
+        Task<IEnumerable<job>> GetOpenJobsAsync();
+        Task<job?> GetJobByIdAsync(long jobId);
+        Task<IEnumerable<job>> GetJobsCreatedByAsync(long creatorId);
+        Task CreateJobAsync(job job, long creatorId);
+        Task UpdateJobAsync(job job);
 
     }
 }
