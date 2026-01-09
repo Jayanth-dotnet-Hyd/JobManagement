ec5979be737a64f534416fccc22df45b3ea648b3
Jayanth Bandi <bandijayanthh@gmail.com>
2026-01-09 15:02:05 +0530

    Added Repository Library

---
+++ JobManagement.Repositories/IRoleRepository.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using JobManagement.Applicant.Data.Models;
+
+
+namespace JobManagement.Repositories
+{
+    internal interface IRoleRepository
+    {
+        
+        
+    }
+}
