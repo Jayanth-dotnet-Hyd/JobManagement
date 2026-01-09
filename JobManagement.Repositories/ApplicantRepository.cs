3849ab8914d2a79a236e353b946ec6dfe4480f7e
sangram-dotnet-pune <sangramcanva7812@gmail.com>
2026-01-09 15:51:05 +0530

    Added Applicant and Job Repository

--- JobManagement.Repositories/ApplicantRepository.cs
+++ JobManagement.Repositories/ApplicantRepository.cs
@@ -1,4 +1,7 @@
-﻿using System;
+﻿using JobManagement.Applicant.Data.Context;
+using JobManagement.Applicant.Data.Models;
+using Microsoft.EntityFrameworkCore;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
@@ -6,7 +9,62 @@ using System.Threading.Tasks;
 
 namespace JobManagement.Repositories
 {
-    internal class ApplicantRepository
+    public class ApplicantRepository : IApplicantRepository
     {
+
+        private readonly JobManagementDbContext _context;
+
+       
+        private const int ApplicantRoleId = 1;
+
+      public  ApplicantRepository(JobManagementDbContext context)
+        {
+            _context = context;
+        }
+
+        public async Task<user?> GetByIdAsync(long id)
+        {
+            return await _context.users
+                .Include(u => u.role)
+                .FirstOrDefaultAsync(u =>
+                    u.id == id &&
+                    u.role_id == ApplicantRoleId);
+        }
+
+        public async Task<user?> GetByEmailAsync(string email)
+        {
+            return await _context.users
+                .Include(u => u.role)
+                .FirstOrDefaultAsync(u =>
+                    u.email == email &&
+                    u.role_id == ApplicantRoleId);
+        }
+
+        public async Task<IEnumerable<user>> GetAllAsync()
+        {
+            return await _context.users
+                .Where(u => u.role_id == ApplicantRoleId)
+                .ToListAsync();
+        }
+
+        public async Task AddAsync(user applicant)
+        {
+            _context.users.Add(applicant);
+            await _context.SaveChangesAsync();
+        }
+
+        public async Task UpdateAsync(user applicant)
+        {
+            _context.users.Update(applicant);
+            await _context.SaveChangesAsync();
+        }
+
+        public async Task<bool> ExistsAsync(string email)
+        {
+            return await _context.users
+                .AnyAsync(u =>
+                    u.email == email &&
+                    u.role_id == ApplicantRoleId);
+        }
     }
 }
--- JobManagement.Repositories/JobRepository.cs
+++ JobManagement.Repositories/JobRepository.cs
@@ -1,4 +1,7 @@
-﻿using System;
+﻿using JobManagement.Applicant.Data.Context;
+using JobManagement.Applicant.Data.Models;
+using Microsoft.EntityFrameworkCore;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
@@ -8,5 +11,54 @@ namespace JobManagement.Repositories
 {
     internal class JobRepository
     {
+        private readonly JobManagementDbContext _context;
+
+        public JobRepository(JobManagementDbContext context)
+        {
+            _context = context;
+        }
+
+        public async Task<IEnumerable<job>> GetAllAsync()
+        {
+            return await _context.jobs
+                .Include(j => j.created_byNavigation)
+                .ToListAsync();
+        }
+
+        public async Task<job?> GetByIdAsync(long id)
+        {
+            return await _context.jobs
+                .Include(j => j.applications)
+                .Include(j => j.created_byNavigation)
+                .FirstOrDefaultAsync(j => j.id == id);
+        }
+
+        public async Task<IEnumerable<job>> GetOpenJobsAsync()
+        {
+            return await _context.jobs
+                .Where(j => j.status == "OPEN")
+                .ToListAsync();
+        }
+
+        //public async Task<IEnumerable<job>> GetJobsByCreatorAsync(long creatorId)
+        //{
+        //    return await _context.jobs
+        //        .Where(j => j.created_by == creatorId)
+        //        .ToListAsync();
+        //}
+
+        public async Task AddAsync(job job)
+        {
+            job.created_at = DateTime.UtcNow;
+            _context.jobs.Add(job);
+            await _context.SaveChangesAsync();
+        }
+
+        public async Task UpdateAsync(job job)
+        {
+            job.updated_at = DateTime.UtcNow;
+            _context.jobs.Update(job);
+            await _context.SaveChangesAsync();
+        }
     }
 }
